290186f6833d014a9418165c4db5967487400b47
Aminekham <lkham0508@gmail.com>
2024-01-04 17:16:45 +0100

    documentation

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+""""
+This is a way to solve the canunlockall problem
+"""
+
 
 def canUnlockAll(boxes):
+    """
+    idea: collecting all the found keys and intrepret 
+    if we can open all the boxes by following any giving 
+    way or not
+    comparaison is the key to verify those keys
+    """
     keys_for_now = []
     c = 1
     for i in range(len(boxes)):
