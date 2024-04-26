a47704fd9890fb25c00ddae88d23c3bb91c7b0cf
Aminekham <lkham0508@gmail.com>
2024-04-26 08:00:12 +0200

    fixing

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -4,22 +4,22 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 request(url, async (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-        const characters = JSON.parse(body).characters;
-        for (const characterUrl of characters) {
-            await new Promise((resolve, reject) => {
-                request(characterUrl, (error, response, body) => {
-                    if (!error && response.statusCode === 200) {
-                        console.log(JSON.parse(body).name);
-                        resolve();
-                    } else {
-                        console.error('Error:', error);
-                        reject();
-                    }
-                });
-            });
-        }
-    } else {
-        console.error('Error:', error);
+  if (!error && response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    for (const characterUrl of characters) {
+      await new Promise((resolve, reject) => {
+        request(characterUrl, (error, response, body) => {
+          if (!error && response.statusCode === 200) {
+            console.log(JSON.parse(body).name);
+            resolve();
+          } else {
+            console.error('Error:', error);
+            reject();
+          }
+        });
+      });
     }
+  } else {
+    console.error('Error:', error);
+  }
 });
