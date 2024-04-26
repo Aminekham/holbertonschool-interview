46767e35de40316567b0d093611f3df1ed102c80
Aminekham <lkham0508@gmail.com>
2024-04-26 07:43:39 +0200

    algorithm

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,26 @@
+import request from 'request';
+
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+request(url, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+    } else if (response.statusCode !== 200) {
+        console.error('Status:', response.statusCode);
+    } else {
+        const data = JSON.parse(body);
+        const characters = data.characters;
+        characters.forEach(characterUrl => {
+            request(characterUrl, (error, response, body) => {
+                if (error) {
+                    console.error('Error:', error);
+                } else if (response.statusCode !== 200) {
+                    console.error('Status:', response.statusCode);
+                } else {
+                    const characterData = JSON.parse(body);
+                    console.log(characterData.name);
+                }
+            });
+        });
+    }
+});
