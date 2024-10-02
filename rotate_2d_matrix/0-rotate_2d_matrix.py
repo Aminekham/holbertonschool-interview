ea6d6207de5f3628e48100cc30f18a0f9e565cf9
Aminekham <lkham0508@gmail.com>
2024-10-02 11:12:56 +0200

    fixing pep8

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,7 +9,7 @@ def rotate_2d_matrix(matrix):
     Simple 2d matrix rotation using
     basic python lists operations
     """
-    n = len(matrix)    
+    n = len(matrix)
     for i in range(n // 2):
         for j in range(i, n - i - 1):
             temp = matrix[i][j]
