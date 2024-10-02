66313cf86b4ed45a92e7a1c3dbeb53004f3d8ac0
Aminekham <lkham0508@gmail.com>
2024-10-02 11:10:25 +0200

    rotating a 2d matrix

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def rotate_2d_matrix(matrix):
+    """
+    Simple 2d matrix rotation using
+    basic python lists operations
+    """
     n = len(matrix)    
     for i in range(n // 2):
         for j in range(i, n - i - 1):
