8f9fa7831840d82ff93c3fa1ab53005865f001a4
Aminekham <lkham0508@gmail.com>
2024-10-02 10:57:32 +0200

    fixing pep8

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -2,11 +2,12 @@
 import requests
 
 def count_words(subreddit, word_list, after='', word_count={}):
-    headers = {'User-Agent': 'python:word.count:v1.0 (by /u/your_username)'}
+    headers = {'User-Agent': 'python:word.count:v1.0'}
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     params = {'limit': 100, 'after': after}
     try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        response = requests.get(url, headers=headers, params=params,
+                                allow_redirects=False)
         if response.status_code != 200:
             return
         data = response.json()
@@ -15,13 +16,16 @@ def count_words(subreddit, word_list, after='', word_count={}):
             title = article['data']['title'].lower().split()
             for word in word_list:
                 lower_word = word.lower()
-                word_count[lower_word] = word_count.get(lower_word, 0) + title.count(lower_word)
+                x = word_count.get(lower_word, 0)
+                y = title.count(lower_word)
+                word_count[lower_word] = x + y
         after = data.get('data', {}).get('after', None)
         if after:
             return count_words(subreddit, word_list, after, word_count)
         else:
             sorted_word_count = sorted(
-                [(word, count) for word, count in word_count.items() if count > 0],
+                [(word, count) for word,
+                 count in word_count.items() if count > 0],
                 key=lambda x: (-x[1], x[0])
             )
             for word, count in sorted_word_count:
