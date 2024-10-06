de69fb7197f88af557623cdd5c65b7d72608b17d
Aminekham <lkham0508@gmail.com>
2024-10-06 16:24:55 +0200

    fixing documentation

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -3,8 +3,7 @@
 
 
 def island_perimeter(grid):
-    """
-    [summary]
+    """[summary]
 
     Args:
         grid ([type]): [description]
@@ -12,6 +11,7 @@ def island_perimeter(grid):
     Returns:
         [type]: [description]
     """
+
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
