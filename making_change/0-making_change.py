13c13e576f9fa349a2c493bfd91f6c7b59301c69
Aminekham <lkham0508@gmail.com>
2024-10-06 16:05:02 +0200

    testing

---
+++ making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+def makeChange(coins, total):
+    """
+    Using the dynamic programming
+    most effective approach
+    """
+    if total <= 0:
+        return 0
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            if dp[amount - coin] != float('inf'):
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    return dp[total] if dp[total] != float('inf') else -1
